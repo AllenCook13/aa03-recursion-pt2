59dacf86c2f84a578c53dc36ce4b539e434d7c0a
AllenCook <albcook1313@hotmail.com>
2024-02-21 16:32:51 -0700

    new repo

--- problems/05-reverse.js
+++ problems/05-reverse.js
@@ -11,13 +11,28 @@ reverse("q"); // "q"
 reverse("id"); // "di"
 reverse(""); // ""
 ***********************************************************************/
+function reverse(arr) {
 
-
-function reverse(str) {
-  // Your code here 
 }
 
+// function reverse(arr) {
+  
+//   if (arr === '') {
+//     return '';
+//   } else {
+//     return arr.push(arr.pop())
+//   }
+  
 
+// }
+
+console.log(reverse(['h','o','u','s','e'])); // "esuoh"
+console.log(reverse("house")); // "esuoh"
+// console.log(reverse("dog")); // "god"
+// console.log(reverse("atom")); // "mota"
+// console.log(reverse("q")); // "q"
+// console.log(reverse("id")); // "di"
+// console.log(reverse("")); // ""
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
   module.exports = reverse;
